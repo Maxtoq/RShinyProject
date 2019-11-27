573f9a678c4aafe90658c570141453aea7324eea
Maxtoq <maxime.toquebiau@hotmail.fr>
2019-11-27 18:08:15 +0100

    finish map

--- app.R
+++ app.R
@@ -5,17 +5,35 @@ library(leaflet)
 states <- geojsonio::geojson_read("json/us-states-clear.geojson", what = "sp")
 data("USArrests")
 
-bins <- c(0, 2, 4, 6, 8, 10, 12, 14, 16, Inf)
-pal <- colorBin("YlOrRd", domain = USArrests$Murder, bins = bins)
+bins_Murder <- c(0, 2, 4, 6, 8, 10, 12, 14, 16, Inf)
+pal_Murder <- colorBin("YlOrRd", domain = USArrests$Murder, bins = bins_Murder)
+
+bins_Assault <- c(0, 50, 100, 150, 200, 250, 300, 350, Inf)
+pal_Assault <- colorBin("YlOrRd", domain = USArrests$Assault, bins = bins_Assault)
+
+bins_Rape <- c(0, 5, 10, 15, 20, 25, 30, 35, 40, Inf)
+pal_Rape <- colorBin("YlOrRd", domain = USArrests$Rape, bins = bins_Rape)
 
 ui <- fluidPage(
-  leafletOutput("mymap"),
-  p()
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Murder",
+               leafletOutput("map_Murder")
+               ), 
+      tabPanel("Assault",
+               leafletOutput("map_Assault")
+               ), 
+      tabPanel("Rape",
+               leafletOutput("map_Rape")
+               )
+    )
+  )
 )
 
 server <- function(input, output, session) {
   
-  output$mymap <- renderLeaflet({
+  output$map_Murder <- renderLeaflet({
     labels <- sprintf(
       "<strong>%s</strong><br/>%g murder arrests / 100 000",
       rownames(USArrests), USArrests$Murder
@@ -27,7 +45,7 @@ server <- function(input, output, session) {
         id = "mapbox.light",
         accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
       addPolygons(
-        fillColor = ~pal(USArrests$Murder),
+        fillColor = ~pal_Murder(USArrests$Murder),
         weight = 2,
         opacity = 1,
         color = "white",
@@ -44,8 +62,80 @@ server <- function(input, output, session) {
           style = list("font-weight" = "normal", padding = "3px 8px"),
           textsize = "15px",
           direction = "auto")) %>%
-      addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
+      
+    addLegend(pal = pal_Murder, values = ~density, opacity = 0.7, title = NULL,
+              position = "bottomright")
+    
+  })
+  
+  output$map_Assault <- renderLeaflet({
+    labels <- sprintf(
+      "<strong>%s</strong><br/>%g assault arrests / 100 000",
+      rownames(USArrests), USArrests$Assault
+    ) %>% lapply(htmltools::HTML)
+    
+    leaflet(states) %>%
+      setView(-96, 37.8, 4) %>%
+      addProviderTiles("MapBox", options = providerTileOptions(
+        id = "mapbox.light",
+        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+      addPolygons(
+        fillColor = ~pal_Assault(USArrests$Assault),
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(
+          weight = 5,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto")) %>%
+      
+      addLegend(pal = pal_Assault, values = ~density, opacity = 0.7, title = NULL,
                 position = "bottomright")
+    
+  })
+  
+  output$map_Rape <- renderLeaflet({
+    labels <- sprintf(
+      "<strong>%s</strong><br/>%g assault arrests / 100 000",
+      rownames(USArrests), USArrests$Rape
+    ) %>% lapply(htmltools::HTML)
+    
+    leaflet(states) %>%
+      setView(-96, 37.8, 4) %>%
+      addProviderTiles("MapBox", options = providerTileOptions(
+        id = "mapbox.light",
+        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+      addPolygons(
+        fillColor = ~pal_Rape(USArrests$Rape),
+        weight = 2,
+        opacity = 1,
+        color = "white",
+        dashArray = "3",
+        fillOpacity = 0.7,
+        highlight = highlightOptions(
+          weight = 5,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto")) %>%
+      
+      addLegend(pal = pal_Rape, values = ~density, opacity = 0.7, title = NULL,
+                position = "bottomright")
+    
   })
 }
 
